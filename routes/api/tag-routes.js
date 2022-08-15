83c7e2a7683f7df96ac4118b02d7807a0e23b54e
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-15 14:37:21 +1000

    Fix tag and product routes to go through product-tags table

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products' }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products' }],
     });
 
     if (!tagData) {
